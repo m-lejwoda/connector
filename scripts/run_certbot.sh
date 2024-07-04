8543e120f99129a20df7d9d08c2b84c1ac04bd63
michal <michal.lejwoda@gmail.com>
2024-07-04 22:47:53 +0200

    run certbot

---
+++ scripts/run_certbot.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+certbot certonly --webroot -w /var/www/certbot --force-renewal --email michal.lejwoda@gmail.com -d true-effects.pl --agree-tos
+certbot certonly --webroot -w /var/www/certbot --force-renewal --email michal.lejwoda@gmail.com -d language-flashcards.pl --agree-tos
+certbot certonly --webroot -w /var/www/certbot --force-renewal --email michal.lejwoda@gmail.com -d scrapper-omi.pl --agree-tos
+certbot certonly --webroot -w /var/www/certbot --force-renewal --email michal.lejwoda@gmail.com -d trener-personalny-michal.pl --agree-tos
